f4c616b1c85c13178719e4f274a6769bee09a652
ajpvc2040 <79205376+ajpvc2040@users.noreply.github.com>
2021-04-29 09:58:59 -0500

    react+aws

--- src/main/java/com/supersoft/thehood/controller/BuddyController.java
+++ src/main/java/com/supersoft/thehood/controller/BuddyController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("api/")
 public class BuddyController {
 
--- src/main/java/com/supersoft/thehood/controller/DebitController.java
+++ src/main/java/com/supersoft/thehood/controller/DebitController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("api/")
 public class DebitController {
 
